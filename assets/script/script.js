beff01e06117b30f667c606d99fb9b2a041fd0b4
Toacin <toacinp@outlook.com>
2022-10-16 16:42:23 -0700

    webpage now allows you to choose which month to check rather than opening flights for next 3 months

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,17 +1,41 @@
-$mainButton = document.querySelector('#mainBTN');
-$allButtons = document.querySelectorAll('.hiddenbtn');
+const $headings = document.querySelectorAll('h3');
+const $loader = document.querySelector('#loader')
+const $mainButton = document.querySelector('#mainBTN');
+const $allLinks = document.querySelectorAll('.hiddenBTN');
+const $monthsButtons = document.querySelectorAll('.monthsBTN');
 
 function clickHandler() {
     let checkedDaysArray = [];
-    $allButtons.forEach((element, index) => {
-        let currentDay = moment();
-        checkedDaysArray.push(currentDay.add((7*index)+1, 'days').format('YYYY-MM-DD'));
-    })
-    $allButtons.forEach((element, index) => {
-        element.setAttribute("href", `https://www.kayak.com/flights/DAC-PHX/${checkedDaysArray[index]}-flexible-3days?fs=cfc=1;bfc=2&sort=bestflight_a`);
-        element.setAttribute("target", "_blank");
-        element.click();
-    });
+    $loader.style.display = "inline-block";
+    let secondsLeft = 1;
+    setInterval(function () {
+        secondsLeft --;
+        if (secondsLeft === 0) {
+            clearInterval();
+            $mainButton.style.display = "none";
+            $headings[0].style.display = "none";
+            $headings[1].style.display = "inherit";
+            $monthsButtons.forEach((element) => {
+                element.style.display = "inherit";
+            })
+            $allLinks.forEach((element, index) => {
+                const currentDay = moment();
+                checkedDaysArray.push(currentDay.add((7*index)+1, 'days').format('YYYY-MM-DD'));
+            })
+            $allLinks.forEach((element, index) => {
+                element.setAttribute("href", `https://www.kayak.com/flights/DAC-PHX/${checkedDaysArray[index]}-flexible-3days?fs=cfc=1;bfc=2&sort=bestflight_a`);
+                element.setAttribute("target", "_blank");
+                // element.click();
+            });
+            $monthsButtons.forEach((button, i) => {
+                button.addEventListener("click", () => {
+                    for (j=0; j<4; j++) {
+                        $allLinks[j+(i*4)].click();
+                    }
+                })
+            });
+        }
+    }, 1500);
 }
 
 $mainButton.addEventListener("click", clickHandler);
